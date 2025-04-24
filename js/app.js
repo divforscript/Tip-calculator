2f13f2d2421724f2abdb781e507bba11123ac35b
Dayvid Ramos <d.ramosm97@gmail.com>
2025-04-23 19:06:54 -0500

    refact: Change inputs handle refactFunctions
    description:
    * Both: bill-input-wrapper and people-input-wrapper
    
      > handleInitialZero:
        - Prevent inserts several zero's as first inputs
        - If first input is ".", change to "0."
        - Avoid insert "-0.xxx" decimal type
    
      > handleNotNumberInput:
        - Change box styles to "error mode" if the
          string is a <Not-Number> input
    
    * Once: people-input-wrapper
    
      >> Note: Use an error flag on implementions
    
      > handleZeroPeople
        - Change box styles if "Number of people" input
          is zero.
    
      > handleNonInteger
        - Prevent to enter "Non-integer" or "Negative" values as
          "Number of people"

--- js/app.js
+++ js/app.js
@@ -1,62 +1,74 @@
 const rootVar = document.documentElement.style;
 const peopleZero = document.querySelector("#people-zero");
 
+let pressedKey = ""
+let inputStr = ""
+let inputNum = 0
+
 // FUNCTIONS
-const onKeyDownFunc = function (event,inputType) {
+// Both: bill-input-wrapper and people-input-wrapper
+const handleInitialZero = (event,pressKey,inputStr,inputNum) => {
+  //for onkeydown events
+  if(pressKey==="." && inputNum===0){
+    event.target.value = "0"
+    return
+  }
 
-  const pressedKey = Number(event.key)
-
-  const billInputStr = event.target.value
-  const billInput = Number(billInputStr)
-
-  console.log(billInputStr)
-
-  
-  if(pressedKey + billInput === 0) {
+  if(pressKey==="0" && (inputStr==="" || inputStr==="0")){
     event.target.value = ""
+  }
+}
 
-    if(inputType === "people"){
-      peopleZero.classList.remove("hide-div")
-      rootVar.setProperty("--peopleFocusBorderColor","var(--errorFocus)")
-    }
 
+const handleNotNumberInput = (number,alertDiv,inputFocus) => {
+  //for onkeyup events
+  if(!(number+1)||number<0) {
+    alertDiv.classList.remove("hidden-div")
+    rootVar.setProperty(inputFocus,"var(--errorColor)")
+    return
+  } 
+  
+  alertDiv.classList.add("hidden-div")
+  rootVar.setProperty(inputFocus,"var(--okColor)")
+  
+}
+
+
+// Once: people-input-wrapper
+const handleZeroPeople = (number,alertDiv) => {
+  if(number===0) {
+    alertDiv.classList.remove("hidden-div")
+    peopleZeroErrorFlag = 1
+    // rootVar.setProperty(inputFocus,"var(--errorFocus)")
     return
   }
+  alertDiv.classList.add("hidden-div")
+  peopleZeroErrorFlag = 0
+  // rootVar.setProperty(inputFocus,"var(--okFocus)")
+}
 
-  if(billInputStr === "0"){
-    event.target.value = billInputStr.replace("0","")
-    peopleZero.classList.add("hide-div")
-    rootVar.setProperty("--peopleFocusBorderColor","var(--okFocus)")
-  }
+const handleNonInteger = (number,alertDiv) => {
 
-} 
-
-
-const onKeyUpFunc = function (event,currProp,currentErrorDiv) {
-
-  let input = Number(event.target.value)
-  console.log(input)
-
-  let currentProperty = "--peopleFocusBorderColor"
-  if(currProp === "bill") currentProperty = "--billFocusBorderColor"
-
-  if(input >= 0){
-    rootVar.setProperty(currentProperty,"var(--okFocus)")
-    currentErrorDiv.classList.add("hide-div")
+  if(number<0 || (number !== Math.floor(number))) {
+    alertDiv.classList.remove("hidden-div")
+    // rootVar.setProperty(inputFocus,"var(--errorFocus)")
+    peopleNotNumberErrorFlag = 1
     return
   }
-
-  rootVar.setProperty(currentProperty,"var(--errorFocus)")
-  currentErrorDiv.classList.remove("hide-div")
-
+  
+  
+  alertDiv.classList.add("hidden-div")
+  peopleNotNumberErrorFlag = 0
+  // rootVar.setProperty(inputFocus,"var(--okFocus)")
 }
 
 
 
-// Handle number inputs behavior
+// SECTION: bill-input-wrapper
 const billInputWrapper = document.querySelector("#bill-input-wrapper");
 const billNotNumber = document.querySelector("#bill-not-number")
 const billMount = document.querySelector("#bill-mount");
+
 
 
 billInputWrapper.onclick = () => {
@@ -64,31 +76,67 @@ billInputWrapper.onclick = () => {
 }
 
 billMount.onkeydown = (event) => {
-  onKeyDownFunc(event,"bill")
+  
+  pressedKey = event.key
+  inputStr = event.target.value
+  inputNum = Number(inputStr)
+
+  handleInitialZero(event,pressedKey,inputStr,inputNum)
 }
 
 billMount.onkeyup = (event) => {
-  onKeyUpFunc(event,"bill",billNotNumber)
+
+  pressedKey = Number(event.key)
+  inputStr = event.target.value
+  inputNum = Number(inputStr)
+
+  handleNotNumberInput(inputNum,billNotNumber,"--billBorderColor")
 }
 
 
 
-
+// SECTION: people-input-wrapper
 const peopleInputWrapper = document.querySelector("#people-input-wrapper");
 // const peopleZero = document.querySelector("#people-zero");
 const peopleNotNumber = document.querySelector("#people-not-number")
 const peopleNum = document.querySelector("#people-num");
+let peopleZeroErrorFlag = 0
+let peopleNotNumberErrorFlag = 0
+
 
 peopleInputWrapper.onclick = () => {
   peopleNum.focus()
 }
 
+
 peopleNum.onkeydown = (event) => {
-  onKeyDownFunc(event,"people")
+  
+  pressedKey = event.key
+  inputStr = event.target.value
+  inputNum = Number(inputStr)
+
+  handleInitialZero(event,pressedKey,inputStr,inputNum)
 }
 
+
 peopleNum.onkeyup = (event) => {
-  onKeyUpFunc(event,"people",peopleNotNumber)
+
+  pressedKey = Number(event.key)
+  inputStr = event.target.value
+  inputNum = Number(inputStr)
+
+  console.log(inputStr)
+  console.log(inputNum)
+
+  handleNotNumberInput(inputNum,peopleNotNumber,"--peopleBorderColor")
+
+  handleZeroPeople(inputNum,peopleZero)
+
+  handleNonInteger(inputNum,peopleNotNumber)
+
+  peopleZeroErrorFlag || peopleNotNumberErrorFlag ? rootVar.setProperty("--peopleBorderColor","var(--errorColor)") : rootVar.setProperty("--peopleBorderColor","var(--okColor)")
+  
+
 }
 
 
@@ -108,62 +156,95 @@ peopleNum.onkeyup = (event) => {
 
 
 // INITIAL VERSION
-// // Handle number inputs behavior
 // const rootVar = document.documentElement.style;
+// const peopleZero = document.querySelector("#people-zero");
 
+// // FUNCTIONS
+// const handleInitialZero = function (event,inputType) {
+
+//   const pressedKey = Number(event.key)
+
+//   const billInputStr = event.target.value
+//   const billInput = Number(billInputStr)
+
+//   console.log(billInputStr)
+
+  
+//   if(pressedKey + billInput === 0) {
+//     event.target.value = ""
+
+//     if(inputType === "people"){
+//       peopleZero.classList.remove("hide-div")
+//       rootVar.setProperty("--peopleFocusBorderColor","var(--errorFocus)")
+//     }
+
+//     return
+//   }
+
+//   if(billInputStr === "0"){
+//     event.target.value = billInputStr.replace("0","")
+//     peopleZero.classList.add("hide-div")
+//     rootVar.setProperty("--peopleFocusBorderColor","var(--okFocus)")
+//   }
+
+// } 
+
+
+// const handleNotNumbers = function (event,currProp,currentErrorDiv) {
+
+//   let input = Number(event.target.value)
+//   console.log(input)
+
+//   let currentProperty = "--peopleFocusBorderColor"
+//   if(currProp === "bill") currentProperty = "--billFocusBorderColor"
+
+//   if(input >= 0){
+//     rootVar.setProperty(currentProperty,"var(--okFocus)")
+//     currentErrorDiv.classList.add("hide-div")
+//     return
+//   }
+
+//   rootVar.setProperty(currentProperty,"var(--errorFocus)")
+//   currentErrorDiv.classList.remove("hide-div")
+
+// }
+
+
+
+// // Handle number inputs behavior
 // const billInputWrapper = document.querySelector("#bill-input-wrapper");
 // const billNotNumber = document.querySelector("#bill-not-number")
 // const billMount = document.querySelector("#bill-mount");
+
 
 // billInputWrapper.onclick = () => {
 //   billMount.focus()
 // }
 
-// billMount.onkeydown = function (event) {
-
-//   let pressedKey = Number(event.key)
-
-//   let billInputStr = event.target.value
-//   let billInput = Number(billInputStr)
-
-//   console.log(pressedKey)
-
-//   console.log("billInputStr = ",billInputStr)
-//   console.log("billInput = ",billInput)
-//   console.log(typeof billInputStr)
-
-
-//   if(pressedKey + billInput === 0) {
-//     this.value = ""
-//     return
-//   }
-
-//   if(billInputStr === "0"){
-//     // console.log("cero")
-//     this.value = billInputStr.replace("0","")
-//   }
-// } 
-
-// billMount.onkeyup = function (event) {
-
-//   let billInput = Number(event.target.value)
-//   // console.log(event.target.value)
-//   console.log(billInput)
-  
-//   if(billInput >= 0){
-//     rootVar.setProperty("--focusBorderColor","var(--okFocus)")
-//     billNotNumber.classList.add("hide-div")
-//     return
-//   }
-
-//   rootVar.setProperty("--focusBorderColor","var(--errorFocus)")
-//   billNotNumber.classList.remove("hide-div")
-
+// billMount.onkeydown = (event) => {
+//   handleInitialZero(event,"bill")
 // }
 
-// const peopleInputWrapper = document.querySelector("#people-input-wrapper")
+// billMount.onkeyup = (event) => {
+//   handleNotNumbers(event,"bill",billNotNumber)
+// }
+
+
+
+
+// const peopleInputWrapper = document.querySelector("#people-input-wrapper");
+// // const peopleZero = document.querySelector("#people-zero");
+// const peopleNotNumber = document.querySelector("#people-not-number")
 // const peopleNum = document.querySelector("#people-num");
 
 // peopleInputWrapper.onclick = () => {
 //   peopleNum.focus()
+// }
+
+// peopleNum.onkeydown = (event) => {
+//   handleInitialZero(event,"people")
+// }
+
+// peopleNum.onkeyup = (event) => {
+//   handleNotNumbers(event,"people",peopleNotNumber)
 // }
